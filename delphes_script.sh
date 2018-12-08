83c49d975057da03e3f22278bff93f6ab5656f23
christinaw97 <cwang5@caltech.edu>
2018-12-08 07:42:48 +0100

    initial commit

---
+++ delphes_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+card="/afs/cern.ch/work/c/christiw/programs/DelphesTiming/cards/delphes_card_CMS.tcl"
+#/afs/cern.ch/user/c/christiw/programs/DelphesTiming/DelphesHepMC /afs/cern.ch/user/c/christiw/programs/DelphesTiming/cards/delphes_card_CMS.tcl /afs/cern.ch/work/c/christiw/public/LLP/Delphes_output/run_m50_w0p1.root /afs/cern.ch/work/c/christiw/public/LLP/mg5_amc_output/ppTohToSS1SS2_SS1Tobb_SS2Toveve/Events/run_m50_w0p1/tag_4_pythia8_events.hepmc
+#/afs/cern.ch/user/c/christiw/programs/DelphesTiming/DelphesHepMC $card /afs/cern.ch/work/c/christiw/public/LLP/Delphes_output/run_m50_w0p01.root /afs/cern.ch/work/c/christiw/public/LLP/mg5_amc_output/ppTohToSS1SS2_SS1Tobb_SS2Toveve/Events/run_m50_w0p01/tag_3_pythia8_events.hepmc
+#/afs/cern.ch/user/c/christiw/programs/DelphesTiming/DelphesHepMC $card /afs/cern.ch/work/c/christiw/public/LLP/Delphes_output/run_m50_w0p001.root /afs/cern.ch/work/c/christiw/public/LLP/mg5_amc_output/ppTohToSS1SS2_SS1Tobb_SS2Toveve/Events/run_m50_w0p001/tag_3_pythia8_events.hepmc
+
+/afs/cern.ch/user/c/christiw/programs/DelphesTiming/DelphesHepMC $card /afs/cern.ch/work/c/christiw/public/LLP/Delphes_output/run_m40_w0p1.root /afs/cern.ch/work/c/christiw/public/LLP/mg5_amc_output/ppTohToSS1SS2_SS1Tobb_SS2Toveve/Events/run_m40_w0p1/tag_1_pythia8_events.hepmc
+/afs/cern.ch/user/c/christiw/programs/DelphesTiming/DelphesHepMC $card /afs/cern.ch/work/c/christiw/public/LLP/Delphes_output/run_m40_w0p01.root /afs/cern.ch/work/c/christiw/public/LLP/mg5_amc_output/ppTohToSS1SS2_SS1Tobb_SS2Toveve/Events/run_m40_w0p01/tag_1_pythia8_events.hepmc
+/afs/cern.ch/user/c/christiw/programs/DelphesTiming/DelphesHepMC $card /afs/cern.ch/work/c/christiw/public/LLP/Delphes_output/run_m40_w0p001.root /afs/cern.ch/work/c/christiw/public/LLP/mg5_amc_output/ppTohToSS1SS2_SS1Tobb_SS2Toveve/Events/run_m40_w0p001/tag_1_pythia8_events.hepmc
+
+/afs/cern.ch/user/c/christiw/programs/DelphesTiming/DelphesHepMC $card /afs/cern.ch/work/c/christiw/public/LLP/Delphes_output/run_m30_w0p1.root /afs/cern.ch/work/c/christiw/public/LLP/mg5_amc_output/ppTohToSS1SS2_SS1Tobb_SS2Toveve/Events/run_m30_w0p1/tag_1_pythia8_events.hepmc
+/afs/cern.ch/user/c/christiw/programs/DelphesTiming/DelphesHepMC $card /afs/cern.ch/work/c/christiw/public/LLP/Delphes_output/run_m30_w0p01.root /afs/cern.ch/work/c/christiw/public/LLP/mg5_amc_output/ppTohToSS1SS2_SS1Tobb_SS2Toveve/Events/run_m30_w0p01/tag_1_pythia8_events.hepmc
+/afs/cern.ch/user/c/christiw/programs/DelphesTiming/DelphesHepMC $card /afs/cern.ch/work/c/christiw/public/LLP/Delphes_output/run_m30_w0p001.root /afs/cern.ch/work/c/christiw/public/LLP/mg5_amc_output/ppTohToSS1SS2_SS1Tobb_SS2Toveve/Events/run_m30_w0p001/tag_1_pythia8_events.hepmc
+
+
+
+
