71d84160aaca1b5e16ef08540245665baa5993d6
christinaw97 <cwang5@caltech.edu>
2019-05-29 02:19:36 +0200

    added UFO model

---
+++ models/SM-HS1S2-fourjet/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.29
+# Mathematica version: 11.3.0 for Linux x86 (64-bit) (March 7, 2018)
+# Date: Fri 15 Mar 2019 23:06:09
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ models/SM-HS1S2/coupling_orders.py
@@ -0,0 +1,24 @@
+# This file was automatically created by FeynRules 2.3.29
+# Mathematica version: 11.2.0 for Mac OS X x86 (64-bit) (September 11, 2017)
+# Date: Tue 18 Dec 2018 16:21:16
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+HIG = CouplingOrder(name = 'HIG',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+HIW = CouplingOrder(name = 'HIW',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
